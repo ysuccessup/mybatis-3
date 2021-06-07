438000d68ac1ec9e6245b7749e1c5dbf9ca7e11a
yangfei <yangfei@cheyipai.com>
2021-06-07 17:26:49 +0800

    from mybatis-3.4.x  add cache note

---
+++ src/main/java/org/apache/ibatis/cache/decorators/SoftCache.java
@@ -0,0 +1,134 @@
+/**
+ *    Copyright 2009-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.cache.decorators;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReadWriteLock;
+
+import org.apache.ibatis.cache.Cache;
+
+/**
+ * 软引用缓存装饰器
+ * Soft Reference cache decorator
+ * Thanks to Dr. Heinz Kabutz for his guidance here.
+ *
+ * @author Clinton Begin
+ */
+public class SoftCache implements Cache {
+  // 强引用队列
+  private final Deque<Object> hardLinksToAvoidGarbageCollection;
+  // 被GC回收的 SoftEntry 集合
+  private final ReferenceQueue<Object> queueOfGarbageCollectedEntries;
+  private final Cache delegate;
+  // hardLinksToAvoidGarbageCollection 的大小
+  private int numberOfHardLinks;
+
+  public SoftCache(Cache delegate) {
+    this.delegate = delegate;
+    this.numberOfHardLinks = 256;
+    this.hardLinksToAvoidGarbageCollection = new LinkedList<Object>();
+    this.queueOfGarbageCollectedEntries = new ReferenceQueue<Object>();
+  }
+
+  @Override
+  public String getId() {
+    return delegate.getId();
+  }
+
+  @Override
+  public int getSize() {
+    removeGarbageCollectedItems();
+    return delegate.getSize();
+  }
+
+
+  public void setSize(int size) {
+    this.numberOfHardLinks = size;
+  }
+
+  @Override
+  public void putObject(Object key, Object value) {
+    removeGarbageCollectedItems();
+    delegate.putObject(key, new SoftEntry(key, value, queueOfGarbageCollectedEntries));
+  }
+
+  @Override
+  public Object getObject(Object key) {
+    Object result = null;
+    @SuppressWarnings("unchecked") // assumed delegate cache is totally managed by this cache
+    SoftReference<Object> softReference = (SoftReference<Object>) delegate.getObject(key);
+    if (softReference != null) {
+      result = softReference.get();
+      if (result == null) {
+        // 值为空，表示已经被GC回收，移除缓存
+        delegate.removeObject(key);
+      } else {
+        // 非空，添加到 hardLinksToAvoidGarbageCollection 队列首部，未做key唯一性判断，所以存在重复添加的情况 ，避免被 GC
+        // See #586 (and #335) modifications need more than a read lock 
+        synchronized (hardLinksToAvoidGarbageCollection) {
+          hardLinksToAvoidGarbageCollection.addFirst(result);
+          // 如果长度超出上限，则移除队列尾部的元素
+          if (hardLinksToAvoidGarbageCollection.size() > numberOfHardLinks) {
+            hardLinksToAvoidGarbageCollection.removeLast();
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public Object removeObject(Object key) {
+    removeGarbageCollectedItems();
+    return delegate.removeObject(key);
+  }
+
+  @Override
+  public void clear() {
+    synchronized (hardLinksToAvoidGarbageCollection) {
+      hardLinksToAvoidGarbageCollection.clear();
+    }
+    removeGarbageCollectedItems();
+    delegate.clear();
+  }
+
+  @Override
+  public ReadWriteLock getReadWriteLock() {
+    return null;
+  }
+
+  // 移除已经被 GC 回收的键
+  private void removeGarbageCollectedItems() {
+    SoftEntry sv;
+    while ((sv = (SoftEntry) queueOfGarbageCollectedEntries.poll()) != null) {
+      delegate.removeObject(sv.key);
+    }
+  }
+
+  // ReferenceQueue即这样的一个对象，当一个obj被gc掉之后，其相应的包装类，即ref对象会被放入queue中。
+  private static class SoftEntry extends SoftReference<Object> {
+    private final Object key;
+
+    SoftEntry(Object key, Object value, ReferenceQueue<Object> garbageCollectionQueue) {
+      super(value, garbageCollectionQueue);
+      this.key = key;
+    }
+  }
+
+}
